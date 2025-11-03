696e087405cd4186a6fe844766f03b8615fcb187
Ramchandra Khadka <khadkaramchandra981@gmail.com>
2025-11-04 00:42:22 +0545

    ...

--- communication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StudentPostListCreateView, TeacherAnnouncementListCreateView
-
-urlpatterns = [
-    path("student-posts/", StudentPostListCreateView.as_view(), name="student-posts"),
-    path("teacher-announcements/", TeacherAnnouncementListCreateView.as_view(), name="teacher-announcements"),
-]
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from school-saas!")
-
-
-if __name__ == "__main__":
-    main()
